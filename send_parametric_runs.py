ba0768c1fc6f27a9c934fa673b027b968de2d387
Daniel Abutbul <ligolas@gmail.com>
2020-06-02 13:25:38 +0300

    After z quench seems to work uncommited to send all runs

--- send_parametric_runs.py
+++ send_parametric_runs.py
@@ -42,15 +42,15 @@ for initial_rho in [round(r,2) for r in np.linspace(0.75, 0.85, 11)]:
     quench_single_run_envelope(action, sim_for_quench, desired_rho_or_h=desired_h)
 quench_single_run_envelope('zquench', ' N=900_h=1.0_rhoH=0.75_AF_triangle_ECMC', 0.8)
 
-# rho_H_arr = [0.7, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95]
-# for h in [1, 0.8]:  # , 0.7]:
-#     for n_factor in [3]:  # [1, 2, 3]:
-#         for rho_H in rho_H_arr:
-#             n_row = 30 * n_factor
-#             n_col = 30 * n_factor
-#             send_single_run_envelope(h, n_row, n_col, rho_H, 'square')
-#             n_row = 50 * n_factor
-#             n_col = 18 * n_factor
-#             send_single_run_envelope(h, n_row, n_col, rho_H, 'honeycomb')
-# send_single_run_envelope(h, 60, 60, 0.88, 'square')
-# send_single_run_envelope(h, 100, 36, 0.88, 'honeycomb')
+rho_H_arr = [0.7, 0.85, 0.86, 0.87, 0.88, 0.89, 0.9, 0.91, 0.92, 0.93, 0.94, 0.95]
+for h in [1, 0.8]:  # , 0.7]:
+    for n_factor in [3]:  # [1, 2, 3]:
+        for rho_H in rho_H_arr:
+            n_row = 30 * n_factor
+            n_col = 30 * n_factor
+            send_single_run_envelope(h, n_row, n_col, rho_H, 'square')
+            n_row = 50 * n_factor
+            n_col = 18 * n_factor
+            send_single_run_envelope(h, n_row, n_col, rho_H, 'honeycomb')
+send_single_run_envelope(h, 60, 60, 0.88, 'square')
+send_single_run_envelope(h, 100, 36, 0.88, 'honeycomb')

4a3ede8038593281a84f29239c37ce254b657014
DundeeB <ligolas@gmail.com>
2020-06-17 20:12:34 +0300

    Updated Queue

--- send_env.sh
+++ send_env.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qsub -V -N sending_runs -o out/send.out -e out/send.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N ./send_parametric_runs.py
+qsub -V -N sending_runs -o out/send.out -e out/send.err -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q S ./send_parametric_runs.py

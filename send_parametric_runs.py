14c95c3f8cfcec8c78d18d3823b1d53fd0e57f41
Daniel Abutbul <ligolas@gmail.com>
2020-06-23 17:14:05 +0300

    Added runs to the larger plate seperation direction

--- send_parametric_runs.py
+++ send_parametric_runs.py
@@ -46,16 +46,31 @@ def main():
     #     quench_single_run_envelope(action, sim_for_quench, desired_rho_or_h=desired_h)
     # quench_single_run_envelope('zquench', ' N=900_h=1.0_rhoH=0.75_AF_triangle_ECMC', 0.8)
     #
-    rho_H_arr = [0.5, 0.6, 0.7, 0.75] + [round(r, 2) for r in np.linspace(0.8, 1, int((1 - 0.8) / 0.01) + 1)]
-    for h in [1, 0.8]:
-        for n_factor in [1, 3, 5, 11, 20]:
-            for rho_H in rho_H_arr:
+    # rho_H_arr = [0.5, 0.6, 0.7, 0.75] + [round(r, 2) for r in np.linspace(0.8, 1, int((1 - 0.8) / 0.01) + 1)]
+    # for h in [1, 0.8]:
+    #     for n_factor in [1, 3, 5, 11, 20]:
+    #         for rho_H in rho_H_arr:
+    #             n_row = 30 * n_factor
+    #             n_col = 30 * n_factor
+    #             send_single_run_envelope(h, n_row, n_col, rho_H, 'square')
+    #             n_row = 50 * n_factor
+    #             n_col = 18 * n_factor
+    #             send_single_run_envelope(h, n_row, n_col, rho_H, 'honeycomb')
+
+    # const eta=pi/4*(N/2)*sig^2/A=pi/8*H/sig*rhoH
+    for rho_H_h1 in [0.89, 0.9, 0.91]:
+        h1 = 1
+        eta = np.pi / 8 * (1 + h1) * rho_H_h1
+        for h in [1.1, 1.2, 1.3, 1.4]:
+            rho_H = 8 * eta / np.pi / (1 + h)
+            for n_factor in [1, 3, 5, 11]:
                 n_row = 30 * n_factor
                 n_col = 30 * n_factor
                 send_single_run_envelope(h, n_row, n_col, rho_H, 'square')
                 n_row = 50 * n_factor
                 n_col = 18 * n_factor
                 send_single_run_envelope(h, n_row, n_col, rho_H, 'honeycomb')
+
 
 if __name__ == "__main__":
     main()

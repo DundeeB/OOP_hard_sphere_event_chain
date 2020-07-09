c2906e7eb01c327d8ce96b1d51bdd6e489d80b3c
DundeeB <ligolas@gmail.com>
2020-07-09 16:24:27 +0300

    Bug in os system sending

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -12,7 +12,7 @@ def main():
         err_pwd = prefix + 'out/post_process_' + d + '.err'
         time.sleep(0.5)
         os.system(
-            "qsub -V -v " + os.path.join(prefix, d) + " -N post_process_" + d + " -o " + out_pwd + " -e " + err_pwd +
+            "qsub -V -v sim_path=" + os.path.join(prefix, d) + " -N post_process_" + d + " -o " + out_pwd + " -e " + err_pwd +
             " -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N " + code_prefix + "post_process_env.sh")
 
 

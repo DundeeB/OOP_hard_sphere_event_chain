4eef2742c578956885eedc9243646f82c49ada57
DundeeB <ligolas@gmail.com>
2020-07-09 17:20:16 +0300

    Removed breaks

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -8,9 +8,7 @@ code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
 
 def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
-    j = 0
     for d in sims:
-        j += 1
         out_pwd = prefix + 'out/post_process_' + d + '.out'
         err_pwd = prefix + 'out/post_process_' + d + '.err'
         time.sleep(0.5)
@@ -18,8 +16,6 @@ def main():
             "qsub -V -v sim_path=" + d.replace('=', '\=') + " -N post_process_" + d + " -o " +
             out_pwd + " -e " + err_pwd + " -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N " + code_prefix +
             "post_process_env.sh")
-        if j > 3:
-            break
 
 
 if __name__ == "__main__":

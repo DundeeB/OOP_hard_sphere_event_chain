87bf8bee4b5c7470018fa1759dd6b5e7d1fcfcf0
DundeeB <ligolas@gmail.com>
2020-07-09 15:57:00 +0300

    Forgot sleep

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 prefix = "/storage/ph_daniel/danielab/ECMC_simulation_results2.0/"
 code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
@@ -9,6 +10,7 @@ def main():
     for d in sims:
         out_pwd = prefix + 'out/post_process_' + d + '.out'
         err_pwd = prefix + 'out/post_process_' + d + '.err'
+        time.sleep(0.5)
         os.system(
             "qsub -V -v " + os.path.join(prefix, d) + " -N post_process_" + d + " -o " + out_pwd + " -e " + err_pwd +
             " -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N " + code_prefix + "post_process_env.sh")

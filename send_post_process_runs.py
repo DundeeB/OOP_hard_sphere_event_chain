81121f3b5d65755972e566c0c5da925094a52274
DundeeB <ligolas@gmail.com>
2020-08-17 15:33:35 +0300

    Running only on h=0.8 to not overload ATLAS

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -1,6 +1,7 @@
 #!/Local/cmp/anaconda3/bin/python -u
 import os
 import time
+import re
 
 prefix = "/storage/ph_daniel/danielab/ECMC_simulation_results2.0/"
 code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
@@ -9,6 +10,8 @@ code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
 def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
     for d in sims:
+        if not re.match('.*h=0.8.*', d):
+            continue
         out_pwd = prefix + 'out/post_process_' + d + '.out'
         err_pwd = prefix + 'out/post_process_' + d + '.err'
         time.sleep(0.5)

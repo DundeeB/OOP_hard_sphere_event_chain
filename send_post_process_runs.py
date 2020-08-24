85cb8cf070351f439f674067834e58a64d8f4ab8
DundeeB <ligolas@gmail.com>
2020-08-24 21:21:45 +0300

    updated options

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -2,6 +2,7 @@
 import os
 import time
 import re
+from send_parametric_runs import params_from_name
 
 prefix = "/storage/ph_daniel/danielab/ECMC_simulation_results2.0/"
 code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
@@ -20,11 +21,16 @@ def send_specific_run(sim_name, post_types):
 def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
     for sim_name in sims:
-        if not re.match('.*h=1.0.*', sim_name):
-            continue
-        send_specific_run(["psi23"])  # "psi14", "psi16", "pos"])
+        N, h, rhoH, ic = params_from_name(sim_name)
+        # ["psi23", "psi14", "psi16", "pos"]
+        if h >= 1.0:
+            send_specific_run(["psi23"])
+        if h == 0.8:
+            send_specific_run(["psi23"])
+        if h <= 0.4:
+            send_specific_run(["psi16"])
 
 
 if __name__ == "__main__":
-    # main()
-    send_specific_run("N=8100_h=1.0_rhoH=0.86_AF_triangle_ECMC",["psi23"])
+    main()
+    # send_specific_run("N=8100_h=1.0_rhoH=0.86_AF_triangle_ECMC",["psi23"])

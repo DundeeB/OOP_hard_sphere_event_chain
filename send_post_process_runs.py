cba068073ef7ead7b16d5c95e7d7f24425be32e7
DundeeB <ligolas@gmail.com>
2020-08-24 21:15:00 +0300

    updated parameters

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -7,21 +7,24 @@ prefix = "/storage/ph_daniel/danielab/ECMC_simulation_results2.0/"
 code_prefix = "/srv01/technion/danielab/OOP_hard_sphere_event_chain/"
 
 
+def send_specific_run(sim_name, post_types):
+    for post_type in post_types:
+        out_pwd = prefix + 'out/post_process_' + sim_name + '_' + post_type + '.out'
+        err_pwd = prefix + 'out/post_process_' + sim_name + '_' + post_type + '.err'
+        time.sleep(2.0)
+        os.system("qsub -V -v sim_path=" + sim_name.replace('=', '\=') + ",run_type=" + post_type +
+                  " -N " + post_type + "_" + sim_name + " -o " + out_pwd + " -e " + err_pwd +
+                  " -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N " + code_prefix + "post_process_env.sh")
+
+
 def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
-    for d in sims:
-        if not re.match('.*h=1.0.*', d):
+    for sim_name in sims:
+        if not re.match('.*h=1.0.*', sim_name):
             continue
-        for run_type in ["psi23"]:  # "psi14", "psi16", "pos"
-            out_pwd = prefix + 'out/post_process_' + d + '_' + run_type + '.out'
-            err_pwd = prefix + 'out/post_process_' + d + '_' + run_type + '.err'
-
-            time.sleep(2.0)
-            os.system(
-                "qsub -V -v sim_path=" + d.replace('=', '\=') + ",run_type=" + run_type +
-                " -N " + run_type + "_" + d + " -o " + out_pwd + " -e " + err_pwd +
-                " -l nodes=1:ppn=1,mem=1gb,vmem=2gb -q N " + code_prefix + "post_process_env.sh")
+        send_specific_run(["psi23"])  # "psi14", "psi16", "pos"])
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    send_specific_run("N=8100_h=1.0_rhoH=0.86_AF_triangle_ECMC",["psi23"])

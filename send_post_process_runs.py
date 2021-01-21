b03d4a29ee420fa2aeedf492410759509f884891
DundeeB <ligolas@gmail.com>
2021-01-21 18:26:39 +0200

    Resending only psi first

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -47,8 +47,7 @@ def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
     for sim in sims:
         create_op_dir(sim)
-    default_op = ["psi", "Bragg_S", "Bragg_Sm", "pos"]  # "Ising", "gM", "Density"]
-    #
+    default_op = ["psi"]  # "Bragg_S", "Bragg_Sm", "pos", "Ising", "gM", "Density"]
     f = open(os.path.join(code_prefix, 'post_process_list.txt'), 'wt')
     try:
         writer = csv.writer(f, lineterminator='\n')

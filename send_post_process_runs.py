7562966c5247c8cd1dfee907e2d56553d2a0347a
DundeeB <ligolas@gmail.com>
2021-01-21 18:16:33 +0200

    Sending a larger batch of runs

--- send_post_process_runs.py
+++ send_post_process_runs.py
@@ -47,7 +47,8 @@ def main():
     sims = [d for d in os.listdir(prefix) if d.startswith('N=') and os.path.isdir(os.path.join(prefix, d))]
     for sim in sims:
         create_op_dir(sim)
-    default_op = ["psi"]  # ["gM", "Ising", "Bragg_S", "Bragg_Sm", "pos"]  # , "Density", , ]
+    default_op = ["Bragg_S", "Bragg_Sm", "pos"]  # ["gM", "Ising", ]  # , "Density", , ]
+    # "psi"
     f = open(os.path.join(code_prefix, 'post_process_list.txt'), 'wt')
     try:
         writer = csv.writer(f, lineterminator='\n')
